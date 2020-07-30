82e94209562c64696a733c4715a57480b9f49ea9
Dan <daniel.s.christianson@gmail.com>
2020-07-30 02:29:23 -0500

    updated some content pages, added resume

--- src/rsm/rsm.js
+++ src/rsm/rsm.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import '../App.css';
+import resumePdf from './daniel christianson resume 2020-07-30.pdf';
+
+function IFrame(props){
+    return(
+        <div>
+            <iframe className="Resume-Iframe" name={props.name} src={props.src} /*height={props.height} width={props.width}*/ />
+        </div>
+    );
+}
 
 function Resume() {
     return(
         <div className="Content-div">
-            THIS IS THE RESUME DIV
+            <a href={resumePdf}>Direct Download</a>
+            <br /><br />
+            <IFrame name="resume" src={resumePdf} />
         </div>
     );
 }
