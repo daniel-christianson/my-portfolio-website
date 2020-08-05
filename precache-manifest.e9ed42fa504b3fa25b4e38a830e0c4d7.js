97adb680094036906f48902b0cd974e1aa9c2383
Dan <daniel.s.christianson@gmail.com>
2020-08-04 22:38:30 -0500

    Updates

---
+++ precache-manifest.e9ed42fa504b3fa25b4e38a830e0c4d7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dabc015c19197b9e340dc28d28c39b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26177d6b8f9b3923ec6a",
+    "url": "/static/css/main.1e620a92.chunk.css"
+  },
+  {
+    "revision": "e90925f720e5695b109b",
+    "url": "/static/js/2.1728ae22.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1728ae22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26177d6b8f9b3923ec6a",
+    "url": "/static/js/main.1757947f.chunk.js"
+  },
+  {
+    "revision": "d313b7b53fbecca45471",
+    "url": "/static/js/runtime-main.3b949a3c.js"
+  },
+  {
+    "revision": "3393a3c1c423940f8282f2a8c6934d6f",
+    "url": "/static/media/daniel christianson resume 2020-07-30.3393a3c1.pdf"
+  },
+  {
+    "revision": "ff91f4bf7a70e2e95a1031a58a7992d6",
+    "url": "/static/media/github.ff91f4bf.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.76882b74e2bce04b0ae775805c648204.js"
+  "/precache-manifest.e9ed42fa504b3fa25b4e38a830e0c4d7.js"
 );
 
 self.addEventListener('message', (event) => {
