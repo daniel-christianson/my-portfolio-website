afc7f3691ec78f517c3caea63bb7fd9128859afe
Dan <daniel.s.christianson@gmail.com>
2020-08-04 21:27:09 -0500

    Updates

---
+++ precache-manifest.7d46d74fb32d9c0f97932ee3e39ebc4e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bb3f1dc423a9c08caeff53ae2737cc6",
+    "url": "/my-portfolio-website/index.html"
+  },
+  {
+    "revision": "2c8fa4b00512537bf766",
+    "url": "/my-portfolio-website/static/css/main.60fc60e8.chunk.css"
+  },
+  {
+    "revision": "c225a27c96ff8d9b59d4",
+    "url": "/my-portfolio-website/static/js/2.dd58124c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-portfolio-website/static/js/2.dd58124c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c8fa4b00512537bf766",
+    "url": "/my-portfolio-website/static/js/main.9f3c9a57.chunk.js"
+  },
+  {
+    "revision": "cdc280256bc89d1c5bc6",
+    "url": "/my-portfolio-website/static/js/runtime-main.d7051dc5.js"
+  },
+  {
+    "revision": "3393a3c1c423940f8282f2a8c6934d6f",
+    "url": "/my-portfolio-website/static/media/daniel christianson resume 2020-07-30.3393a3c1.pdf"
+  },
+  {
+    "revision": "ff91f4bf7a70e2e95a1031a58a7992d6",
+    "url": "/my-portfolio-website/static/media/github.ff91f4bf.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.e20a954c1fbe88d3b4f7e3a8a5895702.js"
+  "/my-portfolio-website/precache-manifest.7d46d74fb32d9c0f97932ee3e39ebc4e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-portfolio-website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
